88f4b3bfe828e6830c55b36ac6e55818b663c9d6
aki09 <akshatbhatia05@gmail.com>
2022-01-06 17:34:44 +0530

    here

---
+++ app.py
@@ -0,0 +1,46 @@
+from cv2 import data
+from flask import Flask, render_template, Response, request
+import urllib
+import cv2
+import requests
+
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)
+
+
+def generate_frames():
+    while True:
+
+        # read the camera frame
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+
+        yield(b'--frame\r\n'
+              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/')
+def index():
+
+    key_1 = request.args.get('key1')
+    key_2 = request.args.get('key2')
+    print(key_1)
+    # --> value1
+    print(key_2)
+    # return 'done'
+    # --> value2
+    # print("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC")
+    return render_template('index.html')
+
+
+@app.route('/video/61575d1c85fd1cc36010924b')
+def video():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
